7c3be466e1a028b1e07a10ad341d557fe7ef7f0f
Raving-hash <Raving-hash@foxmail.com>
2021-12-09 15:55:48 +0800

    DFS搜索，找到起点，然后暴力递归。

---
+++ src/Solution.java
@@ -0,0 +1,35 @@
+
+
+class Solution {
+    public boolean exist(char[][] board, String word) {
+            char[] words = word.toCharArray();
+            int k = 0;
+            for(int i = 0; i < board.length; i++){
+                for(int j = 0; j < board[0].length; j++){
+                    if(dfs(board, words, i, j, k)) return true;
+                }
+            }
+
+        return false;
+    }
+    private boolean dfs(char[][] board, char[] words, int i, int j, int k){
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != words[k]) return false;
+        if(k == words.length-1){
+            return true;
+        }
+        board[i][j] = '\0';
+        boolean res =   dfs(board, words, i+1, j, k+1) ||
+                        dfs(board, words, i-1, j, k+1) ||
+                        dfs(board, words, i, j+1, k+1) ||
+                        dfs(board, words, i, j-1, k+1);
+        board[i][j] = words[k];
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        char[][] strs = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        boolean abcced = solution.exist(strs, "ABCCED");
+        System.out.println("abcced = " + abcced);
+    }
+}
